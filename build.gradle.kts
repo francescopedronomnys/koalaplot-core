9a4015b339449ddf0051145bd930ff64b4d34fe7
Francesco Pedron <francesco.pedron@omnys.com>
2022-11-23 16:51:02 +0100

    Build.gradle adapted for inclusion as a kotlin("jvm") module for a desktop app.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,6 @@
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.dokka:dokka-base:_") // needed for dokka custom format config
-    }
-}
-
 plugins {
     kotlin("multiplatform")
-    id("com.android.library")
     id("org.jetbrains.compose")
-    id("org.jetbrains.dokka")
-    id("io.gitlab.arturbosch.detekt")
-    id("convention.publication")
 }
 
 repositories {
@@ -19,22 +9,12 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:_")
-}
-
 group = "io.github.koalaplot"
 version = "0.2.1"
 
 kotlin {
     explicitApi()
     jvm()
-    js(IR) {
-        browser()
-    }
-    android() {
-        publishLibraryVariants("release")
-    }
 
     sourceSets {
         named("commonMain") {
@@ -44,7 +24,8 @@ kotlin {
                 implementation(compose.material)
                 implementation(compose.ui)
                 implementation(compose.animation)
-                implementation(KotlinX.coroutines.core)
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutines")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$kotlinCoroutines")
             }
         }
 
@@ -58,78 +39,6 @@ kotlin {
             dependencies {
                 implementation(compose.material)
             }
-        }
-    }
-}
-
-android {
-    namespace = "io.github.koalaplot"
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 31
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
-}
-
-/**
- * Custom format adds a custom logo
- */
-tasks.register<org.jetbrains.dokka.gradle.DokkaTask>("dokkaCustomFormat") {
-    moduleName.set("Koala Plot Core")
-    outputDirectory.set(buildDir.resolve("docs/api/${project.version}"))
-
-    pluginConfiguration<org.jetbrains.dokka.base.DokkaBase, org.jetbrains.dokka.base.DokkaBaseConfiguration> {
-        customStyleSheets = listOf(file("src/docs/dokka/logo-styles.css"))
-        customAssets = listOf(
-            file("src/docs/assets/images/logo-icon.svg")
-        )
-    }
-}
-
-tasks["build"].dependsOn.add("dokkaCustomFormat")
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + listOf(
-            "-opt-in=kotlin.RequiresOptIn"
-        )
-        jvmTarget = "11"
-    }
-
-    detekt {
-        source = files("src")
-        parallel = true
-        config = files("$rootDir/detekt.yml")
-        buildUponDefaultConfig = true
-    }
-}
-
-afterEvaluate {
-    // https://discuss.kotlinlang.org/t/disabling-androidandroidtestrelease-source-set-in-gradle-kotlin-dsl-script/21448
-    project.extensions.findByType<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension>()?.let { ext ->
-        ext.sourceSets.removeAll { sourceSet ->
-            setOf(
-                // "androidAndroidTestRelease",
-                "androidTestFixtures",
-                "androidTestFixturesDebug",
-                "androidTestFixturesRelease",
-            ).contains(sourceSet.name)
         }
     }
 }
